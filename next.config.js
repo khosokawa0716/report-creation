f9d6dbf228714bfcf249867ab4b62a5d31ea00bc
細川浩太 <k.hosokawa0716@gmail.com>
2023-02-02 01:21:44 +0900

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
+const prefix = process.env.NODE_ENV === 'production' ? '/report-creation' : ''
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+    assetPrefix: prefix,
   },
 }
 
