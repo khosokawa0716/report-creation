ff4b20bb18c2a30f400366bc44a4f0420133c0f5
khosokawa0716 <khosokawa0716@users.noreply.github.com>
2023-02-04 01:40:17 +0000

    deploy: 84128c35b2998634505489a24afa67df9338ef49

--- _next/static/o7BDpJb7L-eY6x71H0x5-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ffd789ebb36447b1.css","static/chunks/pages/index-8a50b24d2e9fd078.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
